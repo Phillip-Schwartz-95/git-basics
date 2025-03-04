e064687fc504210efb34960cc99a6bfc653ef4cc
Phillip Schwartz <philliptschwartz@gmail.com>
2025-03-05 00:19:12 +0200

    Add utils file and change ball color with random sizes

--- js/main.js
+++ js/main.js
@@ -4,18 +4,20 @@ function onBallClick() {
         const ball = document.querySelector('.ball')
         let currentWidth = parseInt(ball.style.width) || 100
         let currentHeight =  parseInt(ball.style.height) || 100
+
+        let newDiam = getRandomInt(20,60)
     
-        currentWidth += 50
-        currentHeight += 50
+        currentWidth += newDiam
+        currentHeight += newDiam
 
         if (currentWidth > 400 || currentHeight > 400) {
             currentWidth = 100
             currentHeight = 100
+        }
     
         ball.style.width = `${currentWidth}px`
         ball.style.height = `${currentHeight}px`
     
         ball.innerText = currentWidth 
-    }
+        ball.style.backgroundColor = getRandomColor()
 }
-
---
+++ js/util.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256); // Random value for red (0-255)
+    const g = Math.floor(Math.random() * 256); // Random value for green (0-255)
+    const b = Math.floor(Math.random() * 256); // Random value for blue (0-255)
+    
+    return `rgb(${r}, ${g}, ${b})`; // Return the color in CSS-friendly RGB format
+}
